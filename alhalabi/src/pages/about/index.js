3f2d639879d4dbd6e1345bd36661c3f6265339c9
AhmadElHalabi10 <ahmd.el.halabi.4@gmail.com>
2023-04-19 11:28:55 +0300

    Halabi-Content-map-icons-for-about

--- alhalabi/src/pages/about/index.js
+++ alhalabi/src/pages/about/index.js
@@ -1,4 +1,5 @@
 import "./about.css";
+import { FaFacebook, FaInstagram, FaWhatsapp } from "react-icons/fa";
 
 function About() {
   return (
@@ -17,6 +18,27 @@ function About() {
         sentence structures, to generate Lorem Ipsum which looks reasonable. The
         generated Lorem Ipsum is therefore always free from repetition, injected
         humour, or non-characteristic words etc.
+      </div>
+      <iframe
+        className="about-map"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d532.7046602785639!2d36.07736764231194!3d34.549229891802845!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x152225d32eb4b109%3A0x390a62cfbb75b19b!2z2KfZhNit2YTYqNmKINmE2YTYqtis2KfYsdip!5e0!3m2!1sen!2slb!4v1681889550925!5m2!1sen!2slb"
+        width="600"
+        height="450"
+        style={{ border: "0" }}
+        allowfullscreen=""
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"
+      ></iframe>
+      <div className="about-icons">
+        <a href="https://www.facebook.com">
+          <FaFacebook />
+        </a>
+        <a href="https://www.instagram.com">
+          <FaInstagram />
+        </a>
+        <a href="https://web.whatsapp.com/">
+          <FaWhatsapp />
+        </a>
       </div>
     </div>
   );
