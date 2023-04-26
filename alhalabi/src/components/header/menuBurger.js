58045dde4bcbcfe351425a6fd1fb765bcf7a196a
malak-wahyb <malakwahyb537@gmail.com>
2023-04-26 20:33:08 +0300

    edit the navbar and make it responsive

---
+++ alhalabi/src/components/header/menuBurger.js
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom"
+
+function MenuSide(){
+<section className="menuSide">
+<ul>
+  <li>
+    <Link>About us</Link>
+  </li>
+  <li>
+    <Link>About us</Link>
+  </li>
+  <li>
+    <Link>About us</Link>
+  </li>
+  <li>
+    <Link>About us</Link>
+  </li>
+  <li>
+    <Link>About us</Link>
+  </li>
+</ul>
+</section>
+}
+export default MenuSide
